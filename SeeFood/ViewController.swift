d1f363c993501f8722640ffbcf8d8b3d88dee5e1
Sergio <spinfinite07@gmail.com>
2024-02-12 12:59:43 -0700

    Classify picture taken.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -37,11 +37,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             guard let ciimage = CIImage(image: userPickedImage) else {
                 fatalError("Could not convert UIImage into CIImage")
             }
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
     }
-
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in guard let results = request.results as? [VNClassificationObservation] else {
+            fatalError("Model failed to process image.")
+            
+        }
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            
+            try! handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+        
+    }
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
         present(imagePicker, animated: true, completion: nil)
